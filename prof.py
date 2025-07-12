54cae610933d85112c849c4aa5c41d4801f9f331
Abhineet-1502 <abhineetkr2007@gmail.com>
2025-07-12 20:53:17 +0530

    no way

--- prof.py
+++ prof.py
@@ -2,7 +2,7 @@ import pandas as pd
 data={
     'Name':['Abhineet','Ashish','Surya','Nitin','Mudit'],
     'Age':[18,19,20,18,19],
-    'City':['New Delhi','Ranchi','Udaypur','Rewadi'],
+    'City':['New Delhi','Ranchi','Udaypur','Rewadi','Dariyaganj'],
     'Employed':['Yes','No','Yes','No','Yes']
 }
 df=pd.DataFrame(data)
