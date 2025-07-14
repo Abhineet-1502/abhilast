0a0f87e740ef29f35476fb425f0391ba7ac289fb
Abhineet-1502 <abhineetkr2007@gmail.com>
2025-07-14 13:02:50 +0530

    booy

--- prof.py
+++ prof.py
@@ -1,9 +1,7 @@
 import pandas as pd
 data={
-    'Name':['Abhineet','Ashish','Surya','Nitin','Mudit'],
-    'Age':[18,19,20,18,19],
-    'City':['New Delhi','Ranchi','Udaypur','Rewadi','Dariyaganj'],
-    'Employed':['Yes','No','Yes','No','Yes']
+    'Years of Experience':[1.1,1.2,1.3,1.4,2.1,2.5,2.3,3.7,1.9,2.8,4.1,5,6.6,4.5],
+    'Salary':[30000,32000,12000,40230,20000,21000,34000,54000,62000,32320,10000,46660,31200,60000]
 }
 df=pd.DataFrame(data)
 print(df)
